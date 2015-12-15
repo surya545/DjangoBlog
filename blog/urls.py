8c4abde34ef05939ecd3326531212729a094c516
Surya <imsurya545@gmail.com>
2015-12-15 21:01:05 +0530

    new post

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [ url(r'^$', views.post_list, name='post_list'), ]
+urlpatterns = [ url(r'^$', views.post_list, name='post_list'),
+	        url(r'^post/new/$', views.post_new, name='post_new'), ]
